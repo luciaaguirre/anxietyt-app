357cd990fcd56cf43f8be97a143758f049f60ffa
Lucia Aguirre <guerrolucia@gmail.com>
2025-03-22 16:37:19 +0100

    feat: nadds some functionality in js

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', () => {
     const input = document.getElementById('input');
     const addBtn = document.getElementById('add-btn') 
     const list = document.getElementById('list')
+
+    const addItem = (event) => {
+        const text = input.ariaValueMax.trim();
+        if(!text) {
+            return;
+        }
+
+        const li =document.createElement('li');
+        li.textContent = text;
+        list.appendChild(li);
+        input.value = '';
+    }
 });
