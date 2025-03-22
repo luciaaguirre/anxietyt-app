ef7f0abf89e26658084b4d044d0afbbee62cfb81
Lucia Aguirre <guerrolucia@gmail.com>
2025-03-22 16:32:52 +0100

    feat: selects elements by id

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('input');
+    const addBtn = document.getElementById('add-btn') 
+    const list = document.getElementById('list')
+});
